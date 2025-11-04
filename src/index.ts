5d84d784fdc5e0a935b1823b089dcac398347793
Maciel Suassuna <macielsuassuna14@gmail.com>
2025-11-03 22:01:34 -0300

    chore: runtime

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
 import { screeningRoutes } from "./modules/screening/screening.routes";
 
+export const runtime = "edge";
+
 const app = new Elysia({ prefix: "/api/v1" })
   .use(cors())
   .use(screeningRoutes)
